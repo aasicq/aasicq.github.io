4467e049f667e237a880333ac04a3a5aca6fea71
aasicq <aasicqzadab@gmail.com>
2021-03-18 22:28:19 +0545

    First commit

---
+++ scripts/main.js
@@ -0,0 +1,41 @@
+//for image flikiring
+let imgHeading = document.querySelector('img')
+imgHeading.onclick = function(){
+    let imgSrc = imgHeading.getAttribute('src')
+    if(imgSrc === "images/img.JPG"){
+        imgHeading.setAttribute('src', 'images/img1.JPG');
+    } else if(imgSrc==='images/img1.JPG') {
+        imgHeading.setAttribute('src', 'images/img2.JPG');
+    } else {
+        imgHeading.setAttribute('src', 'images/img.JPG');
+    }
+}
+
+
+
+//For changing username
+
+let myButton = document.querySelector('button')
+let myHeading = document.querySelector('h1')
+
+function setUserName(){
+    let myName = prompt('What is your name?');
+    if(!myName){
+        setUserName();
+    } else {
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Welcome '+myName+'!';
+    }
+}
+
+if(!localStorage){
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Welcome '+storedName+'!';
+}
+
+myButton.onclick = function(){
+    setUserName();
+}
+
